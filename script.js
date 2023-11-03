25436dbff1d5c43f37e9c5a7f3894f8fad421238
sébastien <sebastien.faure24@orange.fr>
2023-11-03 15:27:16 +0100

    whatyearisitfront

---
+++ script.js
@@ -0,0 +1,8 @@
+
+
+fetch('https://deploy-me-to-vercel-xi.vercel.app/year')
+    .then(response => response.json())
+    .then(data => {
+        document.querySelector('#year').textContent = data.result
+    })
+
